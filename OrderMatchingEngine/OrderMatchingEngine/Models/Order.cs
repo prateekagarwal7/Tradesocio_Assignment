c54f01b1ab81015c9e38dc62ca8b5de5442274b2
Prateek Agarwal <prateekagarwal246@gmail.com>
2026-04-01 03:18:29 +0530

    Initialize core models: Order and OrderBook; add .gitignore to exclude build artifacts

---
+++ OrderMatchingEngine/OrderMatchingEngine/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderMatchingEngine.Models
+{
+    public class Order
+    {
+        public string Id { get; set; }
+        public string Side { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ OrderMatchingEngine/OrderMatchingEngine/Services/OrderBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderMatchingEngine.Services
+{
+    public class OrderBook
+    {
+        public SortedDictionary<decimal, Queue<Order>> BuyOrders;
+        public SortedDictionary<decimal, Queue<Order>> SellOrders;
+
+        public Dictionary<string, Order> OrderMap;
+
+        public OrderBook()
+        {
+            BuyOrders = new SortedDictionary<decimal, Queue<Order>>(
+                Comparer<decimal>.Create((a, b) => b.CompareTo(a)) // DESC
+            );
+
+            SellOrders = new SortedDictionary<decimal, Queue<Order>>(); // ASC
+
+            OrderMap = new Dictionary<string, Order>();
+        }
+    }
+}
